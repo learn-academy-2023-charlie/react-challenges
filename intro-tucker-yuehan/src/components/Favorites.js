5397b9620342f46f9a2edfbd4c42992800c0d45b
Tmacintyre <Tucker.w.macintyre@gmail.com>
2023-04-28 17:17:55 -0400

    react challanges

---
+++ intro-tucker-yuehan/src/components/Favorites.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+const Favorites = () => {
+    return(
+        <>
+        <h2>Top five favorite foods</h2>
+        <ol>
+            <li>Tacos</li>
+            <li>Fried Chicken</li>
+            <li>Chicken Tiki Masala</li>
+            <li>Curry</li>
+            <li>Ramen</li>
+        </ol>
+        
+        
+        </>
+    )
+}
+
+export default Favorites
---
+++ intro-tucker-yuehan/src/components/Profile.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Profile = () => {
+  return (
+    <>
+    <h3>information about you and your partners</h3>
+      <ol>
+        <li>Tucker</li>
+        <li>Justin</li>
+        <li>Yue Han</li>
+      </ol>
+    </>
+  )
+}
+
+export default Profile
