75fdfdb9dc41465c3e729c20026c60a0647def78
RodCato <code@eliment.org>
2023-04-28 15:48:06 -0500

    switch driver

---
+++ intro-crod-aubrey/src/components/Profile.js
@@ -0,0 +1,23 @@
+// ⭐️ Challenge: Favorites
+// As a developer, you are tasked with creating an application to share your favorite things. Decide which favorite things you would like to showcase. When creating a project it is important to think about organization of your code. It is best practice to separate and compartmentalize all the actions in your code into individual components.
+
+// 📚 User Stories
+// As a user, I can see a Header component with the title of your app.
+// As a user, I can see a Profile component with information about you and your partner.
+// As a user, I can see a Footer component with the name of your cohort.
+// As a user, I can see a Favorites component with a list of top five favorites (i.e. top five favorite TV shows, top five favorite songs, top five favorite taco shops).
+// 🏔 Stretch Goals
+// As a user, I can see a browser tab with a customized name and favicon.
+
+
+import React from "react"
+
+
+function Profile() {
+  return (<>
+    <h2><center>Aubrey and Catalino like beer.</center></h2>
+    </>
+  );
+}
+
+export default Profile;
