0ff867000dadabf631a69884dacc18e236b91a0e
m3vance <m3vance@gmail.com>
2023-04-28 13:55:17 -0700

    completed react challenge

---
+++ intro-vance-will/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./components/Header"
+import Profile from "./components/Profile"
+import Footer from "./components/Footer"
+import Favorites from "./components/Favorites"
+
+const App = () => {
+  return (
+    <>
+  <Header />
+  <Profile />
+  <Favorites />
+  <Footer />
+
+    </>
+  ) 
+}
+
+export default App;
---
+++ intro-vance-will/src/components/Profile.js
@@ -0,0 +1,12 @@
+import React from "react"
+const Profile = () => {
+    return (
+        <>
+        <h2>
+            <p>Vance and Will, the OG jumpstart partners!!!</p>
+            </h2>
+        </>
+    )
+}
+
+export default Profile
