8f47b38ba8b1f5bcacd05f9ef9fe50418e557f62
LEARN Academy <lanyuke@hotmail.com>
2023-05-01 11:30:56 -0700

    build react app

---
+++ state-yuehan-vance/src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import button from './components/button'
+
+const App = () => {
+  return (
+    <>
+     <button/>
+    </>
+  )
+}
+
+export default App
---
+++ state-yuehan-vance/src/components/button.js
@@ -0,0 +1,16 @@
+
+
+
+const button = () => {
+  return (
+    <>
+      <h1>  
+       <p>
+        <button>"CLick me!"</button>
+        </p> 
+        </h1>
+    </>
+  )
+}
+
+export default button
