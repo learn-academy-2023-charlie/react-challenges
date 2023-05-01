555cb03e0d1441e96d4ded363989974c9fb799ae
henrike32 <henrique.lambertucci@gmail.com>
2023-05-01 18:26:18 -0400

    henri driver catalino navigator

---
+++ state-henri-catalino/src/components/Box.js
@@ -0,0 +1,17 @@
+
+
+
+import React, { useState } from "react"
+
+const Box = () => {
+  const [color, setColor] = useState["green"] 
+  return (
+  <> 
+    <h1>Square box app</h1>
+    {/* <div className="App-box"> white </div> */}
+  <p></p>
+  </>
+  )
+}
+
+export default Box
