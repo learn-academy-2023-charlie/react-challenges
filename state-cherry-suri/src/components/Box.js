c7d2db2cb305938b9e7dce7c59fcec92c000e2af
cherrythefool <sarahandecherry@gmail.com>
2023-05-01 17:23:09 -0600

    second driver

--- state-cherry-suri/src/components/Box.js
+++ state-cherry-suri/src/components/Box.js
@@ -3,10 +3,16 @@ import '../App.css'
 
 
 const Box = () => {
-  const [color, setColor] = useState("color")
+  const [color, setColor] = useState("white")
+  
+  const handleClick = () => {
+    setColor(//figure out how to rotate colors
+    )
+  }
+
   return (
   <> 
-    <div className="Box"> {color}</div>
+    <div className="Box" onClick={handleClick}> {color}</div>
   </>
   )
 }
