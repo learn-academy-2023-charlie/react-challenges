07e2d47bc59808f9db4a62c0ccee79933efd3090
egonz0216 <egonz0216@gmail.com>
2023-05-01 15:06:41 -0700

    ernesto driving first challenge

---
+++ state-ernesto-aubrey/src/App.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react'
+import './App.css';
+
+const Box = () => {
+  const [color, setColor] = useState("white")
+  const colorChange = () => {
+    setColor(color)
+  }
+  // <button onClick = {addMiles} style = 
+  // {{background-color:"purple", color:"blue"}
+// > add a mile} </button>
+  return (
+    <>
+      <h1>Color Box</h1>
+      <div className="box"></div>
+      <p><button type="button">Color-name</button></p>
+      
+
+    
+    </>
+  )
+
+
+
+
+
+}
+export default Box
