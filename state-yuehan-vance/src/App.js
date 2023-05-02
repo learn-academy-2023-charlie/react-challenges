e8a36154be55eedcaed83e2559a7c45e3cd13928
LEARN Academy <lanyuke@hotmail.com>
2023-05-01 17:37:38 -0700

    change the code with box to actual buttons

--- state-yuehan-vance/src/App.js
+++ state-yuehan-vance/src/App.js
@@ -1,21 +1,8 @@
 import React from "react"
-// import Button from './components/button'
 import Addbox from './components/addbox'
+import Button from "./components/button"
 import './App.css'
 
-// const App = () => {
-//   return (
-//     <>
-//     <h1 style={{ textAlign: 'center' }}>Random the color!</h1>
-//       <br></br>
-//         <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-//           <Button/>
-//           <Button/>
-//           <Button/>
-//         </div>
-//     </>
-//   )
-// }
 
 const App = () => {
   return (
--- state-yuehan-vance/src/components/addbox.js
+++ state-yuehan-vance/src/components/addbox.js
@@ -1,37 +1,20 @@
 import React, {useState} from 'react'
-
+import Button from './button'
 const Addbox = () => {
-  const [string, setString] =useState(" ")
+  const [boxes, setBoxes] =useState([]);
+
   const addingBox = () => {
-    setString(string + "□")
-  }
+    setBoxes([...boxes, <Button key={boxes.length} />]);
+  };
   const rmingBox = () => {
-    setString(string.slice(0, -1))
-  }
+    setBoxes(boxes.slice(0, -1));
+  };
   return (
     <>
-       <p>{string}</p>
+       <p>{boxes}</p>
        <button onClick={addingBox} >Add a box</button>
        <button onClick={rmingBox} >Remove a box</button>
     </>
   )
 }
-
-// const Button = () => {
-//   const [color, setColor] = useState("white")
-//   const allColor = ["red", "orange", "yellow", "green", "blue", "purple", "pink"]
-  
- 
-//   const pickColor =() => {
-//       const randomColorNum = Math.floor(Math.random() * allColor.length)
-//       const randomColor = allColor[randomColorNum]
-//       setColor(randomColor)
-//   }
-// return (
-//   <>
-//       <button onClick={() => pickColor()} style={{ height: 250 , width: 250 , backgroundColor: color, color: "black" }}>{color}</button>
-//   </>
-// )
-// }
-
 export default Addbox
