902446ad39cfeb61eb4654ec6a0b36ba9f1afb42
Romoramel <romoramel@hotmail.com>
2023-05-03 15:20:45 -0700

    getting started

---
+++ props-ramgel-jessica/src/App.js
@@ -0,0 +1,14 @@
+import React from "react" 
+import Dicebox from "./Components/dicebox"
+import "./App.css"
+
+const App = () => {
+  return(
+    <>
+    <h1>Dice Roll</h1>
+    <Dicebox/>
+    </>
+  )
+}
+
+export default App 
---
+++ props-ramgel-jessica/src/Components/dicebox.js
@@ -0,0 +1,12 @@
+import React from "react" 
+
+const Dicebox = () => {
+  return(
+    <>
+    <div className = "box"></div>
+    <h2>Roll</h2>
+    </>
+  )
+}
+
+export default Dicebox
