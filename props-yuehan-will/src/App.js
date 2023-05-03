05328cffaae85f4ef6dc4fb45f73746cf3fd936a
wabowen89 <wabowen89@gmail.com>
2023-05-03 15:20:34 -0700

    Completed first two stories. Will driving, Yue Han navigating

---
+++ props-yuehan-will/src/App.js
@@ -0,0 +1,32 @@
+// imports
+import React, {useState} from "react"
+import Dice from './components/Dice'
+
+
+function App() {
+  
+  // Psuedocode:
+  // Input: none
+  // Output: random number within given array (1-6)
+  // Function Name: diceRoll
+  // calculate random number between 1 and 6 (possible tool: Math.floor and Math.random methods)
+  // return random numerical value generated
+
+  const [dice, setDice] = useState("")
+
+  const diceRoll = () => {
+    setDice(Math.ceil(Math.random() * 6))
+  }
+
+  
+
+
+  return (<>
+   <Dice 
+   diceRoll = {diceRoll}
+   dice = {dice}
+   />
+  </>)
+}
+
+export default App;
---
+++ props-yuehan-will/src/components/Dice.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dice = ({dice, diceRoll}) => { 
+
+    return <button onClick={diceRoll} style = {{backgroundColor: "white", height: 200, width: 200}}>{dice}</button>
+
+}
+
+export default Dice
