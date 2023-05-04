897564ae2c2f2d16176afe8dcafcb6fc1625dbdf
kyloh1996 <kyloh1996@gmail.com>
2023-05-04 14:13:34 -0500

    second commit-kl

--- functional-props-justin-aubrey-kyle/src/App.js
+++ functional-props-justin-aubrey-kyle/src/App.js
@@ -3,15 +3,34 @@ import MenuItems from "./components/MenuItems"
 
 function App() {
   const [menu, setMenu] = useState([
-    {name: "Bananas", ordered: false},
-    {name: "Peanut Butter", ordered: false},
-    {name: "Ice cream", ordered: false},
-    {name: "Chocolate Sauce", ordered: false}
+    {name: "Bananas", ordered: false, quantity: 0, price: 1.5},
+    {name: "Peanut Butter", ordered: false, quantity: 0, price: 3},
+    {name: "Ice cream", ordered: false, quantity: 0, price: 5},
+    {name: "Chocolate Sauce", ordered: false, quantity: 0, price: 2}
   ])
-  const orderItems = (selectedItem) => {
-    console.log(selectedItem);
-    menu[selectedItem].ordered = true
-    setMenu ([...menu])
+  
+  const orderItems = (selectedItem, quantity) => {
+    const updatedMenu = menu.map((item, index) => {
+      if (index === selectedItem) {
+        return {
+          ...item,
+          ordered: true,
+          quantity: quantity,
+          price: item.price
+        };
+      } else {
+        return item;
+      }
+    });
+    setMenu(updatedMenu);
+  }
+
+  const getTotalPrice = () => {
+    let totalPrice = 0;
+    for (let i = 0; i < menu.length; i++) {
+      totalPrice += menu[i].quantity * menu[i].price;
+    }
+    return totalPrice.toFixed(2);
   }
 
   return (
@@ -25,6 +44,7 @@ function App() {
           <MenuItems item={item} key={index} orderItems={orderItems} index={index}/>
         )
         })}
+      <p>Total Price: ${getTotalPrice()}</p>
     </div>
   );
 }
